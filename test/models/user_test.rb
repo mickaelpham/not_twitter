14aa070a465287dd8b57ae92984213ff8d783ae0
Mickael Pham <mickael.pham@gmail.com>
2015-09-15 18:18:09 -0700

    Add a generic test to user model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'validate presence of email' do
+    u = User.new
+    assert_not u.valid?
+  end
 end
